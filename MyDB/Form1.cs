b105f9b9cda4b185318d96f82417c5cd7ee4db75
Бауыржан <@->
2019-05-01 13:44:41 +0600

    Добавьте файлы проекта.

---
+++ MyDB/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace MyDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnectionStringBuilder connection = new SqlConnectionStringBuilder();
+            try
+            {
+            connection.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=c:\users\бауыржан\мой документы\visual studio 2015\Projects\MyDB\MyDB\MyDB.mdf;Integrated Security=True";
+            //connection.Open();
+            label1.Text = "Connection Successful";
+            //connection.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error" + ex);
+            }
+        }
+    }
+}
